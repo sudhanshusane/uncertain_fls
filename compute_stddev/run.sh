52fd8d9f15d20d48ba7e2b6a10289247055bfb87
Sudhanshu <sudhanshu.sane@gmail.com>
2020-11-24 00:40:33 -0700

    Working stddev generator

--- compute_stddev/run.sh
+++ compute_stddev/run.sh
@@ -3,5 +3,6 @@
 FILE="/home/sci/ssane/data/EthaneDiol/ethane_diol.vtk"
 FIELDS=2
 NEIGHBORHOOD=3
+OUTPUT="/home/sci/ssane/data/EthaneDiol/uncertain_ethane_diol.vtk"
 
-./StdDevGen $FILE $FIELDS $NEIGHBORHOOD
+./StdDevGen $FILE $FIELDS $NEIGHBORHOOD $OUTPUT
